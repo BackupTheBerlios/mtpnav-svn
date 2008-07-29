fab73025b4b8e7d35d68df7fcd0b22d44be72f4e
jchabod <jchabod@b465bd4c-5e52-0410-ad86-ceec2985dca8>
2008-07-29 15:18:21 +0000

    queue thread run permanently - experimental progress callback

--- trunk/mtpnavigator/TransferManager.py
+++ trunk/mtpnavigator/TransferManager.py
@@ -39,9 +39,7 @@ class TransferManager():
         transfer_treeview.set_model(self.__model)
 
     def __observe_queue_thread(self, signal, *args):
-        if signal == ProcessQueueThread.SIGNAL_PROGRESSION:
-            text = ('%i%%' % args[0])
-        elif signal == ProcessQueueThread.SIGNAL_QUEUE_CHANGED:
+        if signal == ProcessQueueThread.SIGNAL_QUEUE_CHANGED:
             if DEBUG: debug_trace("notified SIGNAL_QUEUE_CHANGED", sender=self)
             self.__update_model()
         elif signal == ProcessQueueThread.SIGNAL_DEVICE_CONTENT_CHANGED:
@@ -55,6 +53,9 @@ class TransferManager():
         if DEBUG: debug_trace("Updating model.", sender=self)
         self.__model.clear()
         for job in self.__queue:
+            status = job.status
+            if status==self.STATUS_PROCESSING:
+                status += " %i%%" % job.progress
             self.__model.append([job.object_id, job.action, job.description, job.status])
 
     def __queue_job(self, object_id, job_type, description):
@@ -65,16 +66,14 @@ class TransferManager():
             return
 
         self.__queue.append(job)
-        #self.__model.append([job.object_id, job.action, job.description, job.status])
-        self.__update_model()
+        self.__model.append([job.object_id, job.action, job.description, job.status])
 
         trace("queued file %s for %s" % (job.object_id, job.action), sender=self)
 
         # process the queue if not active
-        #gtk.gdk.threads_enter()
-        if not self.__process_queue_thread.is_running():
+        if not self.__process_queue_thread.isAlive():
+            self.__process_queue_thread.setDaemon(True) # allow to start the thread several times
             self.__process_queue_thread.start()
-        #gtk.gdk.threads_leave()
 
     def send_file(self, file_url):
         if DEBUG: debug_trace("request for sending %s" % file_url, sender=self)
@@ -89,14 +88,13 @@ class TransferManager():
         self.__queue_job(file_id, self.ACTION_DEL, file_description)
 
 class ProcessQueueThread(Thread):
-    SIGNAL_PROGRESSION = 1
-    SIGNAL_QUEUE_CHANGED = 2
-    SIGNAL_DEVICE_CONTENT_CHANGED = 3
+    SIGNAL_QUEUE_CHANGED = 1
+    SIGNAL_DEVICE_CONTENT_CHANGED = 2
 
     def __init__(self, device_engine, transfer_manager, _queue):
         self.__device_engine = device_engine
         self.__queue = _queue
-        self.__is_running = False
+        self.__current_job = None
         Thread.__init__(self)
         self.observers = []
 
@@ -106,47 +104,43 @@ class ProcessQueueThread(Thread):
 
     def __device_callback(self, sent, total):
         percentage = round(float(sent)/float(total)*100)
-        self.__notify(self.SIGNAL_PROGRESSION, percentage)
-
-    def is_running(self):
-        return self.__is_running
+        self.__current_job.progress=percentage
+        #FIXME: self.__notify(self.SIGNAL_QUEUE_CHANGED)
 
     def add_observer(self, observer):
         if DEBUG and observer in self.observers:
-            debug_trace("observer alredy registered to transfer manager", sender=self)
+            debug_trace("observer already registered to transfer manager", sender=self)
         else:
             self.observers.append(observer)
 
     def run(self):
-        self.__is_running = True
         if DEBUG: debug_trace("Processing queue thread started ", sender=self)
-
-        todo = [job for job in self.__queue if  job.status == TransferManager.STATUS_QUEUED]
-        while len(todo) > 0:
-            job = todo[0]
-            job.status = TransferManager.STATUS_PROCESSING
-            self.__notify(self.SIGNAL_QUEUE_CHANGED)
-            debug_trace("Processing job %s" % job.object_id, sender=self)
-            try:
-                if job.action == TransferManager.ACTION_SEND:
-                    self.__device_engine.send_file(job.object_id, None) #self.__device_callback)
-                    trace("%s sent successfully" % job.object_id, sender=self)
-                elif job.action == TransferManager.ACTION_DEL:
-                    self.__device_engine.del_file(job.object_id)
-                    trace("file with id %s deleted succesfully" % job.object_id, sender=self)
-                else:
-                    assert False
-                self.__notify(self.SIGNAL_DEVICE_CONTENT_CHANGED)
-                self.__queue.remove(job)
-            except Exception, exc:
-                if DEBUG: debug_trace("Failed to process %s" % job.object_id , sender=self, exception=exc)
-                job.status = TransferManager.STATUS_ERROR
-                job.exception = exc
-            finally:
-                todo = [job for job in self.__queue if  job.status == TransferManager.STATUS_QUEUED]
-        self.__notify(self.SIGNAL_QUEUE_CHANGED)
-        if DEBUG: debug_trace("Processing queue thread finished", sender=self)
-        self.__is_running = False
+        while(True):
+            todo = [job for job in self.__queue if  job.status == TransferManager.STATUS_QUEUED]
+            if len(todo) > 0:
+                job = todo[0]
+                self.__current_job = job
+                job.status = TransferManager.STATUS_PROCESSING
+                self.__notify(self.SIGNAL_QUEUE_CHANGED)
+                debug_trace("Processing job %s" % job.object_id, sender=self)
+                try:
+                    if job.action == TransferManager.ACTION_SEND:
+                        self.__device_engine.send_file(job.object_id, self.__device_callback)
+                        trace("%s sent successfully" % job.object_id, sender=self)
+                    elif job.action == TransferManager.ACTION_DEL:
+                        self.__device_engine.del_file(job.object_id)
+                        trace("file with id %s deleted succesfully" % job.object_id, sender=self)
+                    else:
+                        assert False
+                    self.__notify(self.SIGNAL_DEVICE_CONTENT_CHANGED)
+                    self.__queue.remove(job)
+                except Exception, exc:
+                    if DEBUG: debug_trace("Failed to process %s" % job.object_id , sender=self, exception=exc)
+                    job.status = TransferManager.STATUS_ERROR
+                    job.exception = exc
+                finally:
+                    self.__current_job = None
+                    self.__notify(self.SIGNAL_QUEUE_CHANGED)
 
 class Job():
     def __init__(self, object_id, action, status, description):
@@ -155,3 +149,4 @@ class Job():
         self.status = status
         self.description = description
         self.exception = None
+        self.progress = 0
