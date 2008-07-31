f254e2623269e163432cda9ed7099c4185829454
jchabod <jchabod@b465bd4c-5e52-0410-ad86-ceec2985dca8>
2008-07-31 14:07:11 +0000

    (!) use event instead of condition for locking queue thred - add/remove row in queue model instead of regenereting it

--- trunk/mtpnavigator/TransferManager.py
+++ trunk/mtpnavigator/TransferManager.py
@@ -1,7 +1,7 @@
 import gtk
 import gobject
 from threading import thread
-from threading import Condition
+from Queue import Queue 
 from urlparse import urlparse
 from urllib import url2pathname
 from DeviceEngine import DeviceEngine
@@ -22,60 +22,39 @@ class TransferManager():
     STATUS_ERROR = "blocked on error"
 
     def __init__(self, device_engine, transfer_treeview, notebook):
-        self.__queue = []
-        self.__error_queue = []
+        self.__queue = Queue()
         self.__device_engine = device_engine
-        # needed to block the queue Thread when no data are disponible
-        self.condition = Condition()
-        process_queue_thread = ProcessQueueThread(device_engine, self, self.__queue, self.__error_queue)
+
+        self.__model = TransfertQueueModel()
+
+        process_queue_thread = ProcessQueueThread(device_engine, self, self.__queue, self.__model)
         process_queue_thread.add_observer(self.__observe_queue_thread)
         process_queue_thread.setDaemon(True)
         process_queue_thread.start()
         self.__transfer_treeview = transfer_treeview
         self.__notebook = notebook
 
-        #col = gtk.TreeViewColumn("object_id", gtk.CellRendererText(), text=0)
-        #transfer_treeview.append_column(col)
+        if DEBUG:
+            col = gtk.TreeViewColumn("object_id", gtk.CellRendererText(), text=0)
+            transfer_treeview.append_column(col)
         col = gtk.TreeViewColumn("action", gtk.CellRendererText(), text=1)
         transfer_treeview.append_column(col)
         col = gtk.TreeViewColumn("description", gtk.CellRendererText(), text=2)
         transfer_treeview.append_column(col)
         col = gtk.TreeViewColumn("status", gtk.CellRendererText(), text=3)
         transfer_treeview.append_column(col)
-
-        self.__model = gtk.ListStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
         transfer_treeview.set_model(self.__model)
 
     def __observe_queue_thread(self, signal, *args):
-        if signal == ProcessQueueThread.SIGNAL_QUEUE_CHANGED:
-            if DEBUG: debug_trace("notified SIGNAL_QUEUE_CHANGED", sender=self)
-            self.__update_model()
-        elif signal == ProcessQueueThread.SIGNAL_DEVICE_CONTENT_CHANGED:
+        if signal == ProcessQueueThread.SIGNAL_DEVICE_CONTENT_CHANGED:
             if DEBUG: debug_trace("notified SIGNAL_DEVICE_CONTENT_CHANGED", sender=self)
             self.__device_engine.update_models()
-
-    def __update_model(self):
-        if DEBUG: debug_trace("Updating model.", sender=self)
-        self.__model.clear()
-        for job in self.__queue:
-            status = job.status
-            if status==self.STATUS_PROCESSING:
-                status += " %i%%" % job.progress
-            self.__model.append([job.object_id, job.action, job.description, job.status])
-        for job in self.__error_queue:
-            self.__model.append([job.object_id, job.action, job.description, job.status])
 
     def __queue_job(self, object_id, job_type, description):
         job = Job(object_id, job_type, self.STATUS_QUEUED, description)
 
-        if self.__queue.count(job) > 0: # FIXME: check ignoring the status
-            warning_trace("%s already in queue" % file_url, sender=self)
-            return
-        self.condition.acquire()
-        self.__queue.append(job)
+        self.__queue.put_nowait(job)
         self.__model.append([job.object_id, job.action, job.description, job.status])
-        self.condition.notify() # signal that a new item is available
-        self.condition.release()
 
         trace("queued file %s for %s" % (job.object_id, job.action), sender=self)
         self.__notebook.set_current_page(1)
@@ -97,10 +76,10 @@ class ProcessQueueThread(Thread):
     SIGNAL_QUEUE_CHANGED = 1
     SIGNAL_DEVICE_CONTENT_CHANGED = 2
 
-    def __init__(self, device_engine, transfer_manager, _queue, errors):
+    def __init__(self, device_engine, transfer_manager, _queue, model):
         self.__device_engine = device_engine
         self.__queue = _queue
-        self.__errors = errors
+        self.__errors
         self.__current_job = None
         Thread.__init__(self)
         self.observers = []
@@ -123,34 +102,53 @@ class ProcessQueueThread(Thread):
     def run(self):
         if DEBUG: debug_trace("Processing queue thread started ", sender=self)
         while(True):
-            transfer_manager.condition.aquire()
-            if len(self.__queue) > 0:
-                job = self.__queue[0]
-                self.__current_job = job
-                job.status = TransferManager.STATUS_PROCESSING
-                self.__notify(self.SIGNAL_QUEUE_CHANGED)
-                debug_trace("Processing job %s" % job.object_id, sender=self)
-                try:
-                    if job.action == TransferManager.ACTION_SEND:
-                        self.__device_engine.send_file(job.object_id, self.__device_callback)
-                        trace("%s sent successfully" % job.object_id, sender=self)
-                    elif job.action == TransferManager.ACTION_DEL:
-                        self.__device_engine.del_file(job.object_id)
-                        trace("file with id %s deleted succesfully" % job.object_id, sender=self)
-                    else:
-                        assert False
-                    self.__notify(self.SIGNAL_DEVICE_CONTENT_CHANGED)
-                except Exception, exc:
-                    if DEBUG: debug_trace("Failed to process %s" % job.object_id , sender=self, exception=exc)
-                    job.status = TransferManager.STATUS_ERROR
-                    job.exception = exc
-                    self.__errors.append(job)
-                finally:
-                    self.__queue.remove(job)
-                    self.__current_job = None
-                    self.__notify(self.SIGNAL_QUEUE_CHANGED)
-            transfer_manager.condition.wait()
-            transfer_manager.condition.release()
+            job = self.__queue.get() # will wait until an job is there
+            #todo: remove from model
+            self.__current_job = job
+            job.status = TransferManager.STATUS_PROCESSING
+            #self.__model.remove_job(job) #FIXME: needed to refresh?
+            #self.__model.append(job)
+            debug_trace("Processing job %s" % job.object_id, sender=self)
+            try:
+                if job.action == TransferManager.ACTION_SEND:
+                    self.__device_engine.send_file(job.object_id, self.__device_callback)
+                    trace("%s sent successfully" % job.object_id, sender=self)
+                elif job.action == TransferManager.ACTION_DEL:
+                    self.__device_engine.del_file(job.object_id)
+                    trace("file with id %s deleted succesfully" % job.object_id, sender=self)
+                else:
+                    assert False
+                self.__notify(self.SIGNAL_DEVICE_CONTENT_CHANGED)
+            except Exception, exc:
+                if DEBUG: debug_trace("Failed to process %s" % job.object_id , sender=self, exception=exc)
+                job.status = TransferManager.STATUS_ERROR
+                job.exception = exc
+                self.__model.append(job)
+            finally:
+                self.__model.remove_job(job) #FIXME: needed to refresh?
+                self.__current_job = None
+
+class TransfertQueueModel(gtk.ListStore)
+    self.COL_JOB_ID=0
+    
+    def __init__(self):
+        gtk.ListStore.__init__(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
+    
+    def __get_iter(self, object_id):
+        #FIXME: use treeiter and if to cache rows?
+        it = self.get_iter_first()
+        while it
+            if self.get_value(it, self.self.COL_JOB_ID) == object_id:
+                return it
+            it = self.iter_next()
+        return None        
+        
+    def remove_job(self, job):
+        it = __get_row_iter(job.object_id)
+        if it:
+            self.remove(it)
+        else:
+            notify_warning("trying to remove non existing object %s from model", job.object_id)
 
 class Job():
     def __init__(self, object_id, action, status, description):
