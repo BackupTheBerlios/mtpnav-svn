e74cbb6cd40482bf1392c6c3798d3e5fa3e644d6
jchabod <jchabod@b465bd4c-5e52-0410-ad86-ceec2985dca8>
2008-08-11 14:23:28 +0000

    Setup.py and manifest, ceate data dir and moves glade files

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # 
 # Setup.py for MTPnavigator
 #
+import os
 
 from distutils.core import setup
 
+
+
 setup(name = "MTPnavigator",
-      version = "0.0.2a",
-      description = "Manage your portable media player",
-      author = "Jerome Chabod",
-      author_email = "jerome.chabod@ifrance.com",
-      url = "http://mtpnav.berlios.de",
-      packages = ["mtpnavigator"],
-      data_files=[("", ["mtpnavigator/MTPnavigator.xml"])]
-      )
+    version = "0.0.2a",
+    description = "Manage your portable media player",
+    author = "Jérôme Chabod",
+    author_email = "jerome.chabod@ifrance.com",
+    url = "http://mtpnav.berlios.de",
+    packages = ["mtpnavigator"],
+    scripts = ["bin/mtpnavigator"],
+    package_dir={'mtpnavigator': 'mtpnavigator'},
+    data_files=('share/mtpnavigator', ['data/MTPnavigator.xml']),
+    requires=["PyMTP(>=0.0.4)"]
+)
 
