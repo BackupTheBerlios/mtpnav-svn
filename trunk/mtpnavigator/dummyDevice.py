d85dbd5dbf09038fcd83a916e0b0e68427893917
jchabod <jchabod@b465bd4c-5e52-0410-ad86-ceec2985dca8>
2008-08-25 15:16:06 +0000

    Change to dummydevice + roadmap

--- trunk/mtpnavigator/dummyDevice.py
+++ trunk/mtpnavigator/dummyDevice.py
@@ -8,11 +8,19 @@ ERRMSG_ALREADY_EXIST = "Already exists on the device" #TRANSLATE
 
 
 class DummyDevice():
-    folder_listing = [(1, 0, "Music"), (2, 0 , "System")]
-    file_listing = [(True, 10, 1, "TEST.MP3", "title", "artist", "album", "genre", 1231, 123454),
-                     (True, 11, 2, "system.sys", "", "", "", "", 0, 0)]
-    file_next_id = 12
+    FOLDER_LISTING = [(1, 0, "Music"), (2, 0 , "System"), (3, 1, "Test folder")]
     DEFAULT_MUSIC_FOLDER = 1
+
+    FILE_LISTING = {10: (True, 1, "TEST.MP3", "title", "artist", "album", "genre", 1231, 123454),
+                    11: (True, 2, "system.sys", "", "", "", "", 0, 0),
+                    12: (True, 1, "TEST2.MP3", "title2", "artist2", "album2", "genre2", 2222, 123454),
+                    13: (True, 3, "TEST3.MP3", "title3", "artist3", "album3", "genre3", 3333, 123454),
+                    14: (True, 3, "TEST4.MP3", "title4", "artist4", "album4", "genre4", 4444, 123454)
+                    }
+
+    PLAYLIST_LISTING = {12: (0, "testPL", [10,12,13])}
+
+    object_next_id = 13
 
     def __init__(self):
         pass
@@ -50,25 +58,26 @@ class DummyDevice():
 
     def get_track_listing(self):
         tracks = []
-        for track in self.file_listing:
+        for track_id in self.FILE_LISTING.keys():
+            track=self.FILE_LISTING[track_id]
             m = Metadata()
             if track[0]:
-                m.id = str(track[1])
+                m.id = str(track_id)
                 m.type = TYPE_TRACK
-                m.parent_id = str(track[2])
-                m.filename = track[3]
-                m.title = track[4]
-                m.artist = track[5]
-                m.album = track[6]
-                m.genre = track[7]
-                m.filesize = track[8]
-                m.date = track[9]
+                m.parent_id = str(track[1])
+                m.filename = track[2]
+                m.title = track[3]
+                m.artist = track[4]
+                m.album = track[5]
+                m.genre = track[6]
+                m.filesize = track[7]
+                m.date = track[8]
                 tracks.append(m)
         return tracks
 
     def get_folder_listing(self):
         folders=[]
-        for folder in self.folder_listing:
+        for folder in self.FOLDER_LISTING:
             m = Metadata()
             m.id = str(folder[0])
             m.parent_id = str(folder[1])
@@ -80,22 +89,40 @@ class DummyDevice():
 
     def get_playlist_listing(self):
         playlists=[]
+        for id in self.PLAYLIST_LISTING.keys():
+            playlist=self.PLAYLIST_LISTING[id]
+            m = Metadata()
+            m.id = str(id)
+            m.parent_id = str(playlist[0])
+            m.type = TYPE_PLAYLIST
+            m.filename = str(playlist[1])
+            m.title = str(playlist[1])
+            playlists.append(m)
         return playlists
 
-    def get_tracks_for_playlist(self, playlist):
-        pass
+    def get_tracks_for_playlist(self, playlist_meta):
+        playlist=self.PLAYLIST_LISTING[int(playlist_meta.id)]
+        tracks=[]
+        for track_id in playlist[2]:
+            tracks.append(self.__get_file_metadata(track_id))
+        return tracks
         
     def get_file_listing(self):
         files = []
-        for file in self.file_listing:
-            m = Metadata()
-            m.id = str(file[1])
-            m.type = TYPE_FILE
-            m.parent_id = str(file[2])
-            m.filename = file[3]
-            m.title = file[3]
+        for file_id in self.FILE_LISTING.keys():
+            m = self.__get_file_metadata(file_id)
             files.append(m)
         return files
+        
+    def __get_file_metadata(self, file_id):
+        file=self.FILE_LISTING[file_id]
+        m = Metadata()
+        m.id = str(file_id)
+        m.type = TYPE_FILE
+        m.parent_id = str(file[1])
+        m.filename = file[2]
+        m.title = file[2]
+        return m
         
     def get_diskusage(self):
         return [752,1000]
